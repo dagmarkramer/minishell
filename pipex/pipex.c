e30b008faac94dd4f5ef4ad23687e2030a67acfd
Dagmar Kramer <dkramer@f1r3s5.codam.nl>
2022-04-04 12:04:52 +0200

    status

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: dkramer <dkramer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/12 19:31:23 by dkramer       #+#    #+#                 */
-/*   Updated: 2022/03/28 14:19:26 by dkramer       ########   odam.nl         */
+/*   Updated: 2022/04/04 12:03:02 by dkramer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@
 #include <fcntl.h>
 #include <sys/errno.h>
 
+typedef struct s_list{
+	char **arg;
+	int pipes[2];
+	int type;
+} t_list;
+
 void	error_handling(char *error_message, t_pipex *pipex)
 {
 	free (pipex);
@@ -27,47 +33,50 @@ void	error_handling(char *error_message, t_pipex *pipex)
 	exit(EXIT_FAILURE);
 }
 
-void	childprocess(int *pipefd, char **argv, char **newenv, t_pipex *pipex, int c)
+void	childprocess(int **pipefd, char **argv, char **newenv, t_pipex *pipex, int c, int i)
 {
-	printf("child\n");
-	if (close(pipefd[0]) == -1)
+	// printf("child\n");
+	if (close(pipefd[i][0]) == -1)
 		exit(EXIT_FAILURE);
 	getpathoptions(argv, c, pipex, newenv);
 	pipex->args = malloc(sizeof(char *) * 4);
 	if (!pipex->args)
 		error_handling("malloc", pipex);
+	// printf("%s\n", pipex->ls);
 	pipex->args[0] = pipex->ls;
 	if (pipex->options)
 		pipex->args[1] = pipex->options;
+	// printf("%s\n", pipex->options);	
 	// pipex->fd = open(argv[1], O_RDONLY);
 	// if (pipex->fd != -1)
 	// 	dup2(pipex->fd, 0);
-	if (dup2(pipefd[1], 1) == -1)
+	if (dup2(pipefd[i][1], 1) == -1)
 		error_handling("dup2() error", pipex);
 	ft_execute(pipex, newenv);
 }
 
-// void	parentprocess(int *pipefd, char **argv, char **newenv, t_pipex *pipex, int c)
-// {
-// 	printf("parent\n");
-// 	if (close(pipefd[1]) == -1)
-// 		exit(EXIT_FAILURE);
-// 	getpathoptions(argv, c, pipex, newenv);
-// 	pipex->args = malloc(sizeof(char *) * 4);
-// 	if (!pipex->args)
-// 		error_handling("malloc", pipex);
-// 	pipex->args[0] = pipex->ls;
-// 	if (pipex->options)
-// 		pipex->args[1] = pipex->options;
-// 	if (access(argv[5], F_OK) == 0 && access(argv[5], W_OK))
-// 		error_handling(argv[5], pipex);
-// 	pipex->fd = open(argv[5], O_RDWR | O_CREAT, 0666);
-// 	if (pipex->fd == -1)
-// 		error_handling(argv[5], pipex);
-// 	if (dup2(pipex->fd, 1) == -1 || dup2(pipefd[0], 0) == -1)
-// 		error_handling("dup2() error", pipex);
-// 	ft_execute(pipex, newenv);
-// }
+void	parentprocess(int *pipefd, char **argv, char **newenv, t_pipex *pipex, int c)
+{
+	waitpid(pipex->cpid, NULL, 0);
+	// printf("parent\n");
+	if (close(pipefd[1]) == -1)
+		exit(EXIT_FAILURE);
+	// getpathoptions(argv, c, pipex, newenv);
+	// pipex->args = malloc(sizeof(char *) * 4);
+	// if (!pipex->args)
+	// 	error_handling("malloc", pipex);
+	// pipex->args[0] = pipex->ls;
+	// if (pipex->options)
+	// 	pipex->args[1] = pipex->options;
+	// if (access(argv[5], F_OK) == 0 && access(argv[5], W_OK))
+	// 	error_handling(argv[5], pipex);
+	// // pipex->fd = open(argv[5], O_RDWR | O_CREAT, 0666);
+	// // if (pipex->fd == -1)
+	// // 	error_handling(argv[5], pipex);
+	// if (dup2(pipefd[0], 0) == -1)
+	// 	error_handling("dup2() error", pipex);
+	// ft_execute(pipex, newenv);
+}
 
 // void	childprocess4(int *pipefd, char **argv, char **newenv, t_pipex *pipex)
 // {
@@ -129,10 +138,10 @@ int	main(int argc, char **argv, char **newenv)
 			error_handling("fork", pipex);
 		if (pipex->cpid != 0)
 		{
-			if (close(pipefd[i][1]) == -1)
-				exit(EXIT_FAILURE);
-			if (dup2(pipefd[i][0], 0) == -1)
-				error_handling("dup2() error", pipex);
+			// if (close(pipefd[i][1]) == -1)
+			// 	exit(EXIT_FAILURE);
+			// if (dup2(pipefd[i][0], 0) == -1)
+			// 	error_handling("dup2() error", pipex);
 			// if (access(argv[5], F_OK) == 0 && access(argv[5], W_OK))
 			// 	error_handling(argv[5], pipex);
 			// pipex->fd = open(argv[5], O_RDWR | O_CREAT, 0666);
@@ -140,12 +149,14 @@ int	main(int argc, char **argv, char **newenv)
 			// 	error_handling(argv[5], pipex);
 			// if (dup2(pipex->fd, 1) == -1)
 			// 	error_handling("dup2() error", pipex);
-			waitpid(pipex->cpid, NULL, 0);
+			parentprocess(pipefd[i], argv, newenv, pipex, c);
+			// waitpid(pipex->cpid, NULL, 0);
 		}
 		else
 		{
 			printf("%d\n", c);
-			childprocess(pipefd[i], argv, newenv, pipex, c);
+			childprocess(pipefd, argv, newenv, pipex, c, i);
+			c++;
 		}
 		// if (i < 1)
 		// 	pipefd[i + 1][0] = pipefd[i][1];
