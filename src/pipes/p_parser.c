4addc9cbebf215dee12b3dd25262b6d6d4d8aea4
Dagmar Kramer <dkramer@f1r3s5.codam.nl>
2022-04-14 14:39:49 +0200

    progress

--- src/pipes/p_parser.c
+++ src/pipes/p_parser.c
@@ -1,79 +1,51 @@
 #include "minishell.h"
 
-int	ms_parser(t_list **tokens, t_mini *mini)
+void	find_redirects(char **new, t_mini *mini)
 {
-	t_list *pipes;
-	int		i;
-	char	**new;
-	
-	i = 0;
-	pipes = 0;
-	mini->pipe_before = 0;
-	mini->pipe_after = 0;
-	if (mini->pipes > 0)
-		mini->pipe_after = 1;
-	new = get_args_exec((*tokens));
-	if (!new)
-		ft_disruptive_exit("malloc fail", 333);
-	ms_lstadd_pipe(&pipes, mini, new);
-	while ((*tokens) && (*tokens)->next)
-	{
-		if (((t_token *)(*tokens)->content)->id == Pipe)
-		{
-			i++;
-			if (mini->pipes - i > 0)
-				mini->pipe_after = 1;
-			mini->pipe_before = 1;
-			new = get_args_exec((*tokens));
-			if (!new)
-				ft_disruptive_exit("malloc fail", 333);
-			ms_lstadd_pipe(&pipes, mini, new);
-		}
-		mini->pipe_after = 0;
-		(*tokens) = (*tokens)->next;
-	}
-	return (0);
+	printf("%s\n",new[0]);
 }
 
-int	ms_parser(t_list **tokens, t_mini *mini)
-{
-	t_list *pipes;
-	int		i;
-	char	**new;
+// int	ms_parser(t_list **tokens, t_mini *mini)
+// {
+// 	t_list *pipes;
+// 	int		i;
+// 	char	**new;
 	
-	i = 0;
-	pipes = 0;
-	mini->pipe_before = 0;
-	mini->pipe_after = 0;
-	while ((*tokens) && (*tokens)->next)
-	{
-		if (((t_token *)(*tokens)->content)->id == Pipe)
-		{
-			if (mini->pipes - i > 0)
-				mini->pipe_after = 1;
-			mini->pipe_before = 1;
-			new = get_args_exec((*tokens));
-			if (!new)
-				ft_disruptive_exit("malloc fail", 333);
-			ms_lstadd_pipe(&pipes, mini, new);
-		}
-		mini->pipe_after = 0;
-		(*tokens) = (*tokens)->next;
-		i++;
-	}
-	// ft_printpipes(pipes);
-	return (0);
-}
+// 	i = 0;
+// 	pipes = 0;
+// 	mini->pipe_before = 0;
+// 	mini->pipe_after = 0;
+// 	find_redirects(new, mini);
+// 	while ((*tokens) && (*tokens)->next)
+// 	{
+// 		if (((t_token *)(*tokens)->content)->id == Pipe)
+// 		{
+// 			if (mini->pipes - i > 0)
+// 				mini->pipe_after = 1;
+// 			mini->pipe_before = 1;
+// 			new = get_args_exec((*tokens));
+// 			if (!new)
+// 				ft_disruptive_exit("malloc fail", 333);
+// 			ms_lstadd_pipe(&pipes, mini, new);
+// 			find_redirects(new, mini);
+// 		}
+// 		mini->pipe_after = 0;
+// 		(*tokens) = (*tokens)->next;
+// 		i++;
+// 	}
+// 	// ft_printpipes(pipes);
+// 	return (0);
+// }
 
 void	ms_count(t_list *tokens, t_mini *mini)
 {
 	mini->pipes = 0;
-	mini->tokens = 1;
+	// mini->tokens = 1;
 	while (tokens && tokens->next)
 	{
 		if (((t_token *)tokens->content)->id == Pipe)
 			mini->pipes++;
-		mini->tokens++;
+		// mini->tokens++;
 		tokens = tokens->next;
 	}
 }
