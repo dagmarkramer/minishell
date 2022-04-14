6c1940433ce6b0d0b753de5cf8c025ee91116f94
Oswin Bult <obult@student.codam.nl>
2022-04-14 12:21:42 +0200

    added new execution functions

--- src/pipes/execute.c
+++ src/pipes/execute.c
@@ -2,31 +2,11 @@
 
 typedef struct	s_execute
 {
-	char		**argv;
+	char		**arg;
+	char		**env;
 	int			fd_input;
 	int			fd_output;
 }				t_execute;
-
-void	ft_execute(t_pipex *pipex, char **newenv)
-{
-	int		i;
-	char	*pathjoined;
-
-	i = -1;
-	// printf("%s\n", pipex->ls);
-	while (pipex->path2d[i + 1])
-	{
-		i++;
-		pathjoined = ft_strjoin(pipex->path2d[i], "/");
-		if (!pathjoined)
-			error_handling("malloc", pipex);
-		pathjoined = ft_strjoin(pathjoined, pipex->ls);
-		if (!pathjoined)
-			error_handling("malloc", pipex);
-		execve(pathjoined, pipex->args, newenv);
-	}
-	exit (127);
-}
 
 char	**get_path_options(t_list *envlst)
 {
@@ -35,14 +15,15 @@ char	**get_path_options(t_list *envlst)
 
 	unsplit = ev_getenv("PATH", envlst); // malloced in linked list so do not need to free
 	if (unsplit == NULL)
-		return (0); // exception
+		return (NULL); // exception
 	paths = ft_split(unsplit, ':');
 	ft_malloc_fail_check(path);
 	return (paths);
 }
 
 // this takes place in a child function (forked)
-void	ft_execute(t_execute *exe_info, t_list *envlst)
+// only for non buildins that do not have an absolute path
+void	ft_execute_relative(t_execute *exe_info, t_list *envlst)
 {
 	char	**paths;
 	char	*pathjoined;
@@ -50,15 +31,29 @@ void	ft_execute(t_execute *exe_info, t_list *envlst)
 
 	i = 0;
 	paths = get_path_options(envlst);
+	if (paths == NULL)
+		exit(127);
 	while (paths[i] != NULL)
 	{
 		pathjoined = ft_strjoin(paths[i], "/");		//strjoin leaks ?
 		ft_malloc_fail_check(pathjoined);
-		pathjoined = ft_strjoin(pathjoined, pipex->ls);		// what is mini or pipe->ls ?
+		pathjoined = ft_strjoin(pathjoined, exe_info->arg[0]);
 		ft_malloc_fail_check(pathjoined);
-		execve(pathjoined, pipex->args, newenv);
+		execve(pathjoined, exe_info->arg, exe_info->env);
 		i++;
 	}
 	exit(127);
-	// execve(path, exe_info->argv, env);
+}
+
+void	ft_execute_absolute(t_execute *exe_info)
+{
+	execve(exe_info->arg[0], exe_info->arg, exe_info->env)
+}
+
+// arg is al gezet
+// env wordt hier gezet (in de child functie?)
+// fd input en output zijn al gezet
+void	exe_pre_fork(t_execute *exe_info)
+{
+	if (pipe(pipefd))
 }
