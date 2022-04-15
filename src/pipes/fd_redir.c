e3002f6a3278b78c5fbe675efb0e368842ee6667
Oswin Bult <obult@student.codam.nl>
2022-04-15 13:47:06 +0200

    redirections are taken care of, untested still

--- src/pipes/fd_redir.c
+++ src/pipes/fd_redir.c
@@ -1,19 +1,5 @@
 #include "minishell.h"
 
-// char	**envlist_to_array(t_list *envlst)
-// {
-// 	t_dictionary	*env_item;
-// 	char			**envarray;
-// 	int				lstsize;
-
-// 	lstsize = ft_lstsize(envlst);
-	
-// 	while (envlst)
-// 	{
-
-// 	}
-// }
-
 void	fd_replacer(int *to_replace, int new_fd)
 {
 	if (*to_replace > 2)
@@ -21,11 +7,12 @@ void	fd_replacer(int *to_replace, int new_fd)
 	*to_replace = new_fd;
 }
 
-void	fd_open_and_replace(int *to_replace, char *filename)
+// O_RDWR O_RDONLY O_WRONLY O_APPEND
+void	fd_open_and_replace(int *to_replace, char *filename, int openflags)
 {
 	int	new_fd;
 
-	new_fd = open(filename, O_RDWR);
+	new_fd = open(filename, openflags);
 	if (*to_replace > 2)
 		close(*to_replace);
 	if (new_fd != -1)
@@ -37,19 +24,33 @@ void	fd_redirect(t_execute *info, int index)
 	if (ft_strncmp(info->arg[index], "<<", 2))
 	{
 		// heredoc
+		fd_replacer(info->fd_input, ms_heredoc(info->arg[index + 1]));
 	}
 	else if (ft_strncmp(info->arg[index], ">>", 2))
 	{
 		// append mode
+		fd_open_and_replace(info->fd_output, info->arg[index + 1], O_APPEND);
 	}
 	else if (ft_strncmp(info->arg[index], ">", 1))
 	{
 		// overwrite (write to)
+		fd_open_and_replace(info->fd_output, info->arg[index + 1], O_WRONLY);
 	}
 	else
 	{
 		// read mode
+		fd_open_and_replace(info->fd_input, info->arg[index + 1], O_RDONLY);
 	}
+	// now we shuffle with pointers and free the ones we do not need anymore so the 
+	// redirection and its succesor do not show up in the char array
+	free(info->arg[index]);
+	free(info->arg[index + 1]);
+	while (info->arg[index + 2] != NULL)
+	{
+		info->arg[index] = info->arg[index + 2];
+		index++;
+	}
+	info->arg[index] = info->arg[index + 2];
 }
 
 char	**fd_redirections(t_execute *info)
