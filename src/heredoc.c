7752cf359cf3209f2a44daef27d964f486482912
Oswin Bult <obult@student.codam.nl>
2022-04-21 15:48:18 +0200

    possibly fixed a possible leak in child process caused by not always coppying in ft_stradd and not freeing the line from get_next_line before reusing it (definetely a leak there)

--- src/heredoc.c
+++ src/heredoc.c
@@ -9,14 +9,21 @@
  *	if the heredoc is disrupted the fd will be closed and nothing will be available to read
  */
 
+/*
+ *	ft_stradd is a strjoin version which can be passed one or multiple NUll pointers
+ *	where in the case of two valid pointers strjoin is performed
+ *	and in the case of one valid pointer a ft_strdup will be returned
+ *	in the case of two NULL pointers NULL will be returned
+ */
+
 char	*ft_stradd(char *str1, char *str2)
 {
 	if (str1 == NULL && str2 == NULL)
 		return (NULL);
-	if (str1 == NULL && str2 != NULL)
-		return (str2);
-	if (str1 != NULL && str2 == NULL)
-		return (str1);
+	if (str1 == NULL)
+		return (ft_strdup(str2);
+	if (str2 == NULL)
+		return (ft_strdup(str1);
 	return (ft_strjoin(str1, str2));
 }
 
@@ -56,6 +63,7 @@ static void	child_process(int writefd, char *delimiter)
 		}
 		tmp = final_product;
 		final_product = ft_stradd_with_newline(final_product, line);
+		free(line);
 		ft_malloc_fail_check(final_product);
 		if (tmp != NULL)
 			free(tmp);
