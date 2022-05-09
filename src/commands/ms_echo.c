daa994ffd85c08235990bdc598eae1487db7a2ee
Oswin Bult <oswin.bult@live.nl>
2022-05-10 00:32:32 +0200

    added checks more true to bash for echo with multiple -n args posssible

--- src/commands/ms_echo.c
+++ src/commands/ms_echo.c
@@ -1,4 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ms_echo.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: oswin <oswin@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/09 22:47:49 by oswin         #+#    #+#                 */
+/*   Updated: 2022/05/10 00:30:10 by oswin         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+/*
+ *	Simple function which just prints out an array of strings with spaces
+ *	inbetween.
+ *	Does not end with a newline.
+ */
 
 static void	eo_printarray(char **array)
 {
@@ -14,30 +32,46 @@ static void	eo_printarray(char **array)
 	}
 }
 
+/*
+ *	Count strings with just -n in them or a dash with multiple n's
+ */
+
+int	eo_dash_n_count(char **array)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	while (array[count] && array[count][0] == '-' && array[count][1] == 'n')
+	{
+		i = 1;
+		while (array[count][i])
+		{
+			if (array[1][i] != 'n')
+				return (count);
+			i++;
+		}
+		count++;
+	}
+	return (count);
+ }
+
+/*
+ *	Replication of the BASH echo command.
+ *	Writes out an array without the echo required as first argument.
+ *	Ends with or without a newline based on the presence of -n flag.
+ */
+
 void	ms_echo(char **array)
 {
 	int	i;
-	int	p;
+	int	ncount;
 
 	i = 1;
-	p = 1;
 	if (ft_strncmp(array[0], "echo", 5) != 0)
 		return ;
-	if (array[1] && array[1][0] == '-' && array[1][1] == 'n')
-	{
-		p = 2;
-		while (array[1][i])
-		{
-			if (array[1][i] != 'n')
-				p = 1;
-			i++;
-		}
-	}
-	if (p == 1)
-	{
-		eo_printarray(&array[1]);
+	ncount = eo_dash_n_count(&array[1]);
+	eo_printarray(&array[1 + ncount]);
+	if (ncount == 0)
 		write(1, "\n", 1);
-	}
-	else
-		eo_printarray(&array[2]);
 }
