685441d4e8d74058b742e394189c917b58ab81a8
Oswin Bult <obult@student.codam.nl>
2022-05-09 18:26:24 +0200

    export now supports values with equals signs

--- src/commands/ms_export.c
+++ src/commands/ms_export.c
@@ -2,17 +2,18 @@
 
 void	ms_export(t_mini *mini, char **array)
 {
-	char	**exportvar;
+	char	*key;
+	char	*value;
 	int		i;
 
 	i = 1;
 	ev_sort_alfa(mini->env);
 	while (array[i] != NULL)
 	{
-		exportvar = ft_split(array[i], '=');
-		if (!exportvar)
-			ft_disruptive_exit("malloc fail", 333);
-		if (ev_change_env(exportvar[0], exportvar[1], &mini->env))
+		key = ft_strcdup(array[i], '=');
+		ft_malloc_fail_check(key);
+		value = ft_strdup(&array[i][ft_strclen(array[i], '=') + 1]);
+		if (ev_change_env(key, value, &mini->env))
 			ft_disruptive_exit("malloc fail", 333);
 		i++;
 	}
